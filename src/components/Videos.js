8862936790a4d9fe792f5a967303fad64b96d886
Nathan <nathan.rawson@downlo-dev.com>
2021-01-23 23:42:44 +0000

    edit videos  for mobile

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,42 +1,43 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar'
 import VideoList from './VideoList'
 import VideoDetail from './VideoDetail'
 import useVideos from '../hooks/useVideos'
 import Navbar from './Navbar';
+import './app.css'
 
 const Videos = () => {
- 
- const [selectedVideo, setSelectedVideo] = useState(null);
- const [videos, search] = useVideos('lowfi')
 
- useEffect(() => {
-    setSelectedVideo(videos[0]);
- }, [videos]);
+   const [selectedVideo, setSelectedVideo] = useState(null);
+   const [videos, search] = useVideos('lowfi')
+
+   useEffect(() => {
+      setSelectedVideo(videos[0]);
+   }, [videos]);
 
 
 
    // setSelectedVideo(response.data.items[0])
 
- return (
-    <div>
-   <Navbar/>
- <div className="ui container">
-    
-            <SearchBar onFormSubmit={search}/>
+   return (
+      <div>
+         <Navbar />
+         <div className="ui container">
+
+            <SearchBar onFormSubmit={search} />
             <div className="ui grid">
-                <div className="ui row">
-                    <div className="eleven wide column">
-            <VideoDetail video={selectedVideo}/>
-            </div>
-            <div className="five wide column">
-            <VideoList onVideoSelect ={setSelectedVideo} videos={videos}/>
+               <div className="ui row">
+                  <div id="video" className="eleven wide column">
+                     <VideoDetail video={selectedVideo} />
+                  </div>
+                  <div id="videoList" className="five wide column">
+                     <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
+                  </div>
+               </div>
             </div>
          </div>
-    </div>
- </div>
- </div>
- );
+      </div>
+   );
 
 };
 
