e8e12a778decf28efc9f755286613576dddcecd7
Nathan <nathan.rawson@downlo-dev.com>
2021-01-23 23:14:34 +0000

    fix full screen for youtube

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,25 +1,29 @@
 import React from 'react'
 
-const VideoDetail = ({video}) => {
-    if(video != null){
+const VideoDetail = ({ video }) => {
+    if (video != null) {
 
         const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
 
-    return (
-        <div>
-            <div className="ui embed">
-                <iframe title="video player" src={videoSrc}/>
+        return (
+            <div>
+                <div className="ui embed">
+                    <iframe title="video player" src={videoSrc} allowfullscreen="allowfullscreen"
+                        mozallowfullscreen="mozallowfullscreen"
+                        msallowfullscreen="msallowfullscreen"
+                        oallowfullscreen="oallowfullscreen"
+                        webkitallowfullscreen="webkitallowfullscreen" />
+                </div>
+                <div className="ui segment">
+                    <h4>{video.snippet.title}</h4>
+                    <p>{video.snippet.description}</p>
+                </div>
             </div>
-    <div className="ui segment">
-        <h4>{video.snippet.title}</h4>
-    <p>{video.snippet.description}</p>
-        </div>
-        </div>
-    )
-    }else{
+        )
+    } else {
         return (
             <div>Loading...</div>
-            )
+        )
     }
 }
 
